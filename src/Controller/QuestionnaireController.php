215c1b638405f9e623ed5a30306424a53b68a0e6
tair-alisher <ur.alisher@mail.ru>
2019-01-23 08:41:17 +0600

    symfony framework

---
+++ src/Controller/QuestionnaireController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Remote\Questionnaire;
+
+class QuestionnaireController extends AbstractController
+{
+    /**
+     * @Route("/questionnaires/{page}", name="questionnaires", requirements={"page"="\d+"})
+     */
+    public function index($page = 1)
+    {
+        $limit = 5;
+        $questionnaires = $this->getDoctrine()
+            ->getRepository(Questionnaire::class, 'server')
+            ->getAllQuestionnaires($page, $limit);
+        $totalPages = ceil($questionnaires->count() / $limit);
+        $currentPage = $page;
+
+        return $this->render('questionnaire/index.html.twig', [
+            'questionnaires' => $questionnaires,
+            'totalPages' => $totalPages,
+            'currentPage' => $currentPage
+        ]);
+    }
+}
---
+++ src/Repository/Remote/QuestionnaireRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repository\Remote;
+
+use App\Entity\Remote\Questionnaire;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
+/**
+ * QuestionnaireRepository
+ */
+class QuestionnaireRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Questionnaire::class);
+    }
+
+    public function getAllQuestionnaires($currentPage = 1, $limit = 10) : object
+    {
+        $query = $this->createQueryBuilder('q')
+            ->orderBy('q.title', 'ASC')
+            ->getQuery();
+
+        $paginator = $this->paginate($query, $currentPage, $limit);
+
+        return $paginator;
+        // $query->execute();
+    }
+
+    public function paginate($dql, $page = 1, $limit)
+    {
+        $paginator = new Paginator($dql);
+
+        $paginator->getQuery()
+            ->setFirstResult($limit * ($page - 1))
+            ->setMaxResults($limit);
+
+        return $paginator;
+    }
+}
